73b348013fd749d3f5d0bd39587ab041896ccb92
cristopher-gomez-m <sonrisita011@hotmail.com>
2023-04-06 08:46:31 -0500

    added a Post method to createBook

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Req,Body } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { Request } from 'express';
 
 @Controller('books')
 export class BooksController {
     constructor(private bookService:BooksService){};
 
     @Get()
-    findAll(){
-        return this.bookService.findAll();
+    findAll(@Req() request:Request){
+        return this.bookService.findAll(request.query);
+    }
+    @Get(':bookId')
+    findBook(@Param('bookId') bookId:string){
+        return this.bookService.findBook(bookId);
+    }
+    @Post()
+    createBook(@Body() body){
+        let newBook:any=body;
+        return this.bookService.createBook(newBook);
     }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,7 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BooksService {
-    findAll():any{
-        return 'findAll is working';
+    findAll(params):any{
+        let msg = `findAll funcionando. Parámetros:`;
+
+        if (params.order !== undefined) {
+          msg = msg + ` order: ${params.order}`;
+        }
+    
+        if (params.limit !== undefined) {
+          msg = msg + ` limit: ${params.limit}`;
+        }
+    
+        return msg;
+    }
+
+    findBook(bookId:string){
+        return `findBook funcionado con bookId:${bookId}`;
+    }
+
+    createBook(newBook:any){
+      return newBook;
     }
 }

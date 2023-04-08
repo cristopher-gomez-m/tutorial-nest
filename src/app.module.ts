e5e8d45737ad5704079c0b41d6dc14953db692c6
cristopher-gomez-m <sonrisita011@hotmail.com>
2023-04-08 09:48:50 -0500

    added enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksService } from './books/books.service';
 import { BooksController } from './books/books.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+require('dotenv').config();
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(
+    {
+      "type": "mysql",
+      "host": process.env.DB_HOST,
+      "port": parseInt(process.env.DB_PORT),
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": "tutorial",
+      "entities": ["dist/**/*.entity.js"], 
+      "synchronize": true, 
+    }
+  )],
   controllers: [AppController, BooksController],
   providers: [AppService, BooksService],
 })
